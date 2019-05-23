ff46410f50d9cc900754d6e5f958ad96c131e980
xssdpgy <2234501359@qq.com>
2019-05-23 14:38:17 +0800

    SqlSessionFactoryBuilder.build()解析,用于构建SqlSessionFactory

--- mybatis-3-master/src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
+++ mybatis-3-master/src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -27,11 +27,16 @@ import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 
 /**
  * Builds {@link SqlSession} instances.
- *
+ * 构建SqlSessionFactory的工厂，工厂模式
  * @author Clinton Begin
  */
 public class SqlSessionFactoryBuilder {
+  //SqlSessionFactoryBuilder有9个build()方法，根据入参类型大体分为三类
+  //第1-4种 ：Reader作为解析器构建的入参    1-3 ->4
+  //第5-8种：InputStream作为解析器构建的入参  5-8 ->8
+  //第9种：Configuration作为build方法的入参，并返回一个DefaultSqlSessionFactory，前8种都是构建配置，回流调用该方法  4,8 ->9
 
+  //以下三个方法都是调用第4种方法
   public SqlSessionFactory build(Reader reader) {
     return build(reader, null, null);
   }
@@ -44,9 +49,14 @@ public class SqlSessionFactoryBuilder {
     return build(reader, null, properties);
   }
 
+  //第4种方法是最常用的，它使用了一个参照了XML文档或更特定的SqlMapConfig.xml文件的Reader实例。
+  //可选的参数是environment和properties。Environment决定加载哪种环境(开发环境/生产环境)，包括数据源和事务管理器。
+  //如果使用properties，那么就会加载那些properties（属性配置文件），那些属性可以用${propName}语法形式多次用在配置文件中。
   public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
     try {
+      //委托XMLConfigBuilder来解析xml文件，并构建
       XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+      //解析为配置Configuration，调用第9个build，返回SqlSessionFactory
       return build(parser.parse());
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error building SqlSession.", e);
@@ -60,6 +70,7 @@ public class SqlSessionFactoryBuilder {
     }
   }
 
+  //以下三个方法都是调用第8种方法
   public SqlSessionFactory build(InputStream inputStream) {
     return build(inputStream, null, null);
   }
@@ -72,6 +83,7 @@ public class SqlSessionFactoryBuilder {
     return build(inputStream, null, properties);
   }
 
+  //第8种方法实现和第4种方法实现相同，Reader换成了InputStream
   public SqlSessionFactory build(InputStream inputStream, String environment, Properties properties) {
     try {
       XMLConfigBuilder parser = new XMLConfigBuilder(inputStream, environment, properties);
@@ -88,6 +100,8 @@ public class SqlSessionFactoryBuilder {
     }
   }
 
+  //第9个build方法使用了一个Configuration作为参数,并返回DefaultSqlSessionFactory
+  //前8个方法都是将入参解析为Configuration，最终调用该方法返回SqlSessionFactory
   public SqlSessionFactory build(Configuration config) {
     return new DefaultSqlSessionFactory(config);
   }
