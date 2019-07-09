0730469b45a1c78e20f7fa6d32d74e54dc11169a
xssdpgy <2234501359@qq.com>
2019-07-10 00:31:27 +0800

    datasourse包下 非池化数据源UnpooledDataSource工厂类及对象解析

--- mybatis-3-master/src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSource.java
+++ mybatis-3-master/src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSource.java
@@ -30,18 +30,22 @@ import java.util.logging.Logger;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.io.Resources;
-
 /**
+
+ * 非池化数据源对象
  * @author Clinton Begin
  * @author Eduardo Macarron
  */
 public class UnpooledDataSource implements DataSource {
 
+  //驱动类加载器
   private ClassLoader driverClassLoader;
+  //数据库驱动属性，可选项。特征：属性的前缀是以“driver.”开头的,例如 driver.encoding=UTF8
   private Properties driverProperties;
+  //已注册的驱动  ConcurrentHashMap类型--> key:类名   value：驱动对象
   private static Map<String, Driver> registeredDrivers = new ConcurrentHashMap<>();
 
-  private String driver;
+  private String driver;  //驱动类名
   private String url;
   private String username;
   private String password;
@@ -49,6 +53,7 @@ public class UnpooledDataSource implements DataSource {
   private Boolean autoCommit;
   private Integer defaultTransactionIsolationLevel;
 
+  //初始化registeredDrivers，如果存在多个驱动，则遍历加进 registeredDrivers
   static {
     Enumeration<Driver> drivers = DriverManager.getDrivers();
     while (drivers.hasMoreElements()) {
@@ -182,11 +187,21 @@ public class UnpooledDataSource implements DataSource {
     this.defaultTransactionIsolationLevel = defaultTransactionIsolationLevel;
   }
 
+  /**
+   * 根据用户名和密码获得Connection
+   * @param username
+   * @param password
+   * @return
+   * @throws SQLException
+   */
   private Connection doGetConnection(String username, String password) throws SQLException {
+    //创建 Properties 对象
     Properties props = new Properties();
+    //设置 驱动属性driverProperties 到 props 中
     if (driverProperties != null) {
       props.putAll(driverProperties);
     }
+    //设置 用户名username 和密码password 到 props 中
     if (username != null) {
       props.setProperty("user", username);
     }
@@ -196,25 +211,39 @@ public class UnpooledDataSource implements DataSource {
     return doGetConnection(props);
   }
 
+  /**
+   * 执行获得Connection连接
+   * @param properties
+   * @return
+   * @throws SQLException
+   */
   private Connection doGetConnection(Properties properties) throws SQLException {
+    //初始化 Driver
     initializeDriver();
+    //获得 Connection 对象
     Connection connection = DriverManager.getConnection(url, properties);
+    //配置 Connection 对象
     configureConnection(connection);
     return connection;
   }
 
+  //初始化驱动     synchronized
   private synchronized void initializeDriver() throws SQLException {
+    //判断 registeredDrivers 是否已经存在该 driver ，若不存在，进行初始化
     if (!registeredDrivers.containsKey(driver)) {
       Class<?> driverType;
       try {
+        //获得driver类
         if (driverClassLoader != null) {
           driverType = Class.forName(driver, true, driverClassLoader);
         } else {
           driverType = Resources.classForName(driver);
         }
+        //创建 Driver 对象
         // DriverManager requires the driver to be loaded via the system ClassLoader.
         // http://www.kfu.com/~nsayer/Java/dyn-jdbc.html
         Driver driverInstance = (Driver)driverType.newInstance();
+        //创建 DriverProxy 对象，并注册到 DriverManager 中
         DriverManager.registerDriver(new DriverProxy(driverInstance));
         registeredDrivers.put(driver, driverInstance);
       } catch (Exception e) {
@@ -223,6 +252,7 @@ public class UnpooledDataSource implements DataSource {
     }
   }
 
+  //配置连接属性  autoCommit和defaultTransactionIsolationLevel
   private void configureConnection(Connection conn) throws SQLException {
     if (autoCommit != null && autoCommit != conn.getAutoCommit()) {
       conn.setAutoCommit(autoCommit);
--- mybatis-3-master/src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
+++ mybatis-3-master/src/main/java/org/apache/ibatis/datasource/unpooled/UnpooledDataSourceFactory.java
@@ -25,6 +25,7 @@ import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
 
 /**
+ * 非池化的数据源工厂
  * @author Clinton Begin
  */
 public class UnpooledDataSourceFactory implements DataSourceFactory {
@@ -38,16 +39,25 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
     this.dataSource = new UnpooledDataSource();
   }
 
+  /**
+   * 设置数据源属性
+   * @param properties
+   */
   @Override
   public void setProperties(Properties properties) {
+    //数据库驱动属性 driverProperties，单独设置之后再设置进 MetaObject 对象中
     Properties driverProperties = new Properties();
+    //创建数据源对应的 MetaObject 对象
     MetaObject metaDataSource = SystemMetaObject.forObject(dataSource);
+    //遍历 properties 属性，初始化到 driverProperties 和 MetaObject中
     for (Object key : properties.keySet()) {
       String propertyName = (String) key;
+      //如果属性的前缀是"driver."，说明该属性是数据库驱动属性（driverProperties，可选项），在这里进行判断并设置进数据库驱动属性
       if (propertyName.startsWith(DRIVER_PROPERTY_PREFIX)) {
         String value = properties.getProperty(propertyName);
         driverProperties.setProperty(propertyName.substring(DRIVER_PROPERTY_PREFIX_LENGTH), value);
       } else if (metaDataSource.hasSetter(propertyName)) {
+        //其他属性初始化到 MetaObject 中
         String value = (String) properties.get(propertyName);
         Object convertedValue = convertValue(metaDataSource, propertyName, value);
         metaDataSource.setValue(propertyName, convertedValue);
@@ -55,6 +65,7 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
         throw new DataSourceException("Unknown DataSource property: " + propertyName);
       }
     }
+    // 设置 driverProperties 到 MetaObject 中
     if (driverProperties.size() > 0) {
       metaDataSource.setValue("driverProperties", driverProperties);
     }
@@ -65,9 +76,18 @@ public class UnpooledDataSourceFactory implements DataSourceFactory {
     return dataSource;
   }
 
+  /**
+   * 值转换方法：根据 setter 的类型,将配置文件中的值强转成相应的类型
+   * @param metaDataSource
+   * @param propertyName
+   * @param value
+   * @return
+   */
   private Object convertValue(MetaObject metaDataSource, String propertyName, String value) {
     Object convertedValue = value;
+    //获得该属性的 setter 方法的参数类型
     Class<?> targetType = metaDataSource.getSetterType(propertyName);
+    //强转配置中的值类型  针对Integer，Long，Boolean三种类型判断
     if (targetType == Integer.class || targetType == int.class) {
       convertedValue = Integer.valueOf(value);
     } else if (targetType == Long.class || targetType == long.class) {
